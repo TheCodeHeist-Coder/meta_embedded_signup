d71be9f0ac0c518c9c4c056c6392c354ff4628e3
Rajkumar <thecodeheist01@gmail.com>
2026-06-09 11:29:08 +0530

    pirvacy and policy added in repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import WhatsAppSignup from "./WhatsAppSignup";
+import PrivacyPolicy from "./PrivacyPolicy";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<WhatsAppSignup />} />
+        <Route path="/policies" element={<PrivacyPolicy />} />
       </Routes>
     </Router>
   );
---
+++ src/PrivacyPolicy.tsx
@@ -0,0 +1,12 @@
+
+function PrivacyPolicy() {
+  return (
+    <div>
+        <h1> Privacy & policies </h1>
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia beatae numquam aliquam sit nam veritatis neque repudiandae, necessitatibus dolorem dolore nulla similique quae laudantium reiciendis expedita unde. Tenetur, incidunt sint? </p>
+        <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, nihil. </p>
+    </div>
+  )
+}
+
+export default PrivacyPolicy
